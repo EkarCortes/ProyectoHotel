54f714907efc928d7b64fbfe8e117611264e4038
abiga <abigailmolina201@gmail.com>
2023-08-13 21:45:17 -0600

    Clase Clientes
    Se creo la clase Clientes con sus atributos, sus constructores y sus getter y setter

---
+++ src/Models/Clientes.java
@@ -0,0 +1,43 @@
+package Models;
+
+/**
+ *
+ * @author abiga
+ */
+public class Clientes extends Persona{
+    private String FechadeNacimiento;
+    private String Correo;
+    
+    public Clientes(String identificacion, String nombre, String telefono, String fechaDeNacimiento, String correo) {
+        super(identificacion, nombre, telefono); 
+        this.FechadeNacimiento = fechaDeNacimiento;
+        this.Correo = correo;
+    }
+
+    public Clientes() {
+        super(); 
+        this.FechadeNacimiento = "";
+        this.Correo = "";
+    }
+
+    public Clientes(String identificacion, String nombre, String fechaDeNacimiento) {
+        super(identificacion, nombre); 
+        this.FechadeNacimiento = fechaDeNacimiento;
+        this.Correo = "";
+    }
+     public String getFechaDeNacimiento() {
+        return FechadeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.FechadeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.Correo = correo;
+    }
+}
